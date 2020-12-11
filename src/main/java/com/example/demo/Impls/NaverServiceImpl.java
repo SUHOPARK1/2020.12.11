6e04fda53daa9cd68cd43531c96c5c7659f3dbf0
SUHOPARK1 <qkrtnghsla@naver.com>
2020-12-11 12:09:34 +0900

    first commit

---
+++ src/main/java/com/example/demo/Impls/NaverServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.Impls;
+
+import com.example.demo.services.NaverService;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NaverServiceImpl implements NaverService{
+    
+}
